5e6aaf1d5a015a8e89e50e7e558150cc24db33c6
matheusrmello <matheus.rm.09@hotmail.com>
2024-12-20 10:21:56 -0300

    Updating project

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "test-matheus-tfstate-front-2"
     key    = "frontend/tfstate"
-    dynamodb_table = "test-matheus-terraform-state"
+    dynamodb_table = "test-matheus-terraform-state-frontend"
     region = "us-east-2"
   }
 }
